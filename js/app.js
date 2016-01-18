a238e68fa1a615d63dff595f89f5708e0719c798
EricR <rasliche@gmail.com>
2016-01-18 16:08:43 -0500

    computed properties
    demo of working computed properties, though not sure if it’s a good use
    for them.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
-var vm = new Vue({
+var transactions = [
+  { target: 'Income', category: 'Income', memo: 'Check from A', flow: 351, date: '1/1/16'},
+  { target: 'Debbie', category: 'Rent', flow: -325, date: '1/8/16'},
+  { target: 'Burdines', category: 'Restaurant', flow: -11, date:'1/11/16' },
+  { target: '5 Brothers', category: 'Restaurant', flow: -15, date: '1/11/16'},
+  { target: 'Transfer: Checking', category: '', memo: 'for rent', flow: 320, date: '1/15/16' }
+];
+
+new Vue({
   el: '#app',
 
   data: {
-    newTransaction: '',
-    transactions: []
+    newTransaction: {},
+    transactions: transactions
   },
 
   methods: {
     addTransaction: function() {
       var title = this.newTransaction.trim();
       if (title) {
-        localforage.setItem('title', title);
+        this.transactions.push({title: title});
         this.newTransaction = '';
       }
     },
@@ -18,7 +26,13 @@ var vm = new Vue({
     removeTransaction: function(index) {
       this.transactions.splice(index, 1);
     }
+  },
+
+  computed: {
+    balance: function() {
+      return this.transactions.reduce(function(a, b) {
+        return (a + b.flow);
+      }, 0)
+    }
   }
 })
-
-vm.transactions = ["Take the dog out", "pay bills", "write songs"];
