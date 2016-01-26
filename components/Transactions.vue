221b1af1c3ca65678833559b2354484f6f14228c
EricR <rasliche@gmail.com>
2016-01-26 16:41:21 -0500

    Breaking out of ES6

--- components/Transactions.vue
+++ components/Transactions.vue
@@ -1,24 +1,33 @@
 <template>
 <h1 class="pure-menu-heading">Transactions</h1>
-<table class="pure-table">
+<table class="pure-table pure-table-horizontal">
   <thead>
     <tr>
       <th>Account</th>
       <th>Date</th>
-      <th>Payee</th>
+      <th>Target</th>
       <th>Category</th>
       <th>Memo</th>
       <th>Flow</th>
     </tr>
+    <tr>
+      <th><input placeholder="Account" v-model="newtransaction.account"></th>
+      <th><input style="width:100px;" placeholder="Date" v-model="newtransaction.date"></th>
+      <th><input placeholder="Target"  v-model="newtransaction.target"></th>
+      <th><input placeholder="Category" v-model="newtransaction.category"></th>
+      <th><input placeholder="Memo" v-model="newtransaction.memo"></th>
+      <th><input style="width:60px;" placeholder="Flow" v-model="newtransaction.flow"></th>
+      <th><button v-on:click="addTransaction()"></th>
+    </tr>
   </thead>
   <tbody>
-    <tr>
-      <td>{{ store.state.transactions }}</td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
+    <tr v-for="transaction in transactions">
+      <td>{{ transaction.account }}</td>
+      <td>{{ transaction.date }}</td>
+      <td>{{ transaction.target }}</td>
+      <td>{{ transaction.category }}</td>
+      <td>{{ transaction.memo }}</td>
+      <td>{{ transaction.flow }}</td>
     </tr>
   </tbody>
 </table>
@@ -27,14 +36,21 @@
 <script>
 var store = require('../store.js')
 
-export default {
-  computed: {
-    transactions() {
-      return store.state.transactions
-    }
-  }
+module.exports = {
+   computed: {
+     transactions() {
+       return store.state.transactions
+     },
+     newtransaction() {
+       return store.state.newtransaction
+     }
+   },
+  //  methods: {
+  //    store.actions
+  //  }
 }
 </script>
 
 <style>
+
 </style>
--- store.js
+++ store.js
@@ -1,26 +1,63 @@
-import Vue from 'vue'
+var Vue = require('vue')
 import Vuex from 'vuex'
 
 // Tell Vue to use Vuex
 Vue.use(Vuex)
 
 // Define the mutation variables
-const SOME_MUTATION = 'SOME_MUTATION'
+const ADD_TRANSACTION = 'ADD_TRANSACTION'
 
 // export the store
-export default new Vuex.Store({
+module.exports = new Vuex.Store({
   state: {
-    transactions: [1,2]
+    newtransaction: {
+      account: "",
+      date: "",
+      target: "",
+      category: "",
+      memo: "",
+      flow: 0
+    },
+    transactions: [
+      {
+        account: "YAY",
+        date:"hey"
+      },
+      {
+        account: "Cash",
+        date: "1/1/16",
+        target: "5 Brothers 2",
+        category: "Restaurants",
+        memo: "Breakfast for A+E",
+        flow: -20
+      },
+      {
+        account: "Cash",
+        date: "1/8/16",
+        target: "Debbie",
+        category: "Rent",
+        memo: "+A",
+        flow: -325
+      },
+      {
+        account: "Cash",
+        date: "1/6/16",
+        target: "Money Market Checking",
+        category: "Transfer",
+        memo: "for Rent",
+        flow: 320
+      }
+    ]
   },
 
   mutations: {
-    [SOME_MUTATION] (state) {
-      // mutate state
+    [ADD_TRANSACTION] (state, transaction) {
+      state.transactions.push(transaction)
     }
   },
 
   actions: {
-     // get called by components
+     addTransaction: ADD_TRANSACTION
   },
   strict: true // the anti-intern device
 })
