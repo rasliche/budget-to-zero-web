3fa3ebed089aa01bfb7f27aa5d931cb7b0c711d0
EricR <rasliche@gmail.com>
2016-01-28 19:41:36 -0500

    Bootstrap Form Groups
    Styled the NewTransactionForm using bootstrap classes. Registered with
    the Transactions.vue component only for now.

--- components/NewTransactionForm.vue
+++ components/NewTransactionForm.vue
@@ -1,13 +1,34 @@
 <template>
-  <div class="transaction-entry">
-    <input placeholder="Account" :value="newtransaction.account" @input="updateNewTransaction">
-    <input placeholder="Date" :value="newtransaction.date" @input="updateNewTransaction">
-    <input placeholder="Target"  :value="newtransaction.target" @input="updateNewTransaction">
-    <input placeholder="Category" :value="newtransaction.category" @input="updateNewTransaction">
-    <input placeholder="Memo" :value="newtransaction.memo" @input="updateNewTransaction">
-    <input placeholder="Flow" :value="newtransaction.flow" @input="updateNewTransaction">
-    <button class="btn" v-on:click="addTransaction()">
+<form>
+  <div class="form-group">
+    <label for="newTransactionAccount">Account</label>
+    <input class="form-control" placeholder="Account" :value="newtransaction.account" @input="updateNewTransaction">
   </div>
+  <div class="form-group">
+    <label for="newTransactionAccount">Date</label>
+    <input class="form-control" placeholder="Date" :value="newtransaction.date" @input="updateNewTransaction">
+  </div>
+  <div class="form-group">
+    <label for="newTransactionAccount">Target</label>
+    <input class="form-control" placeholder="Target"  :value="newtransaction.target" @input="updateNewTransaction">
+  </div>
+  <div class="form-group">
+    <label for="newTransactionAccount">Category</label>
+    <input class="form-control" placeholder="Category" :value="newtransaction.category" @input="updateNewTransaction">
+  </div>
+  <div class="form-group">
+    <label for="newTransactionAccount">Memo</label>
+    <input class="form-control" placeholder="Memo" :value="newtransaction.memo" @input="updateNewTransaction">
+  </div>
+  <div class="form-group">
+    <label for="newTransactionFlow">Flow</label>
+    <div class="input-group">
+      <div class="input-group-addon">$</div>
+      <input class="form-control" placeholder="Flow"  :value="newtransaction.flow" @input="updateNewTransaction">
+    </div>
+  </div>
+    <button class="btn btn-default" v-on:click.prevent="addTransaction()">Add Transaction</button>
+</form>
 </template>
 
 <script>
