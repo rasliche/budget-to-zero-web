ece8906dc7b4410d1f153838a40abeb5b5c993aa
EricR <rasliche@gmail.com>
2016-02-01 23:07:28 -0500

    Remove references to addTransaction

--- components/NewTransactionForm.vue
+++ components/NewTransactionForm.vue
@@ -2,32 +2,32 @@
 <form>
   <div class="form-group">
     <label for="newTransactionAccount">Account</label>
-    <input class="form-control" placeholder="Account" :value="newtransaction.account" @input="updateNewTransaction">
+    <input class="form-control" placeholder="Account" v-model="account" @input="">
   </div>
   <div class="form-group">
     <label for="newTransactionAccount">Date</label>
-    <input class="form-control" placeholder="Date" :value="newtransaction.date" @input="updateNewTransaction">
+    <input class="form-control" placeholder="Date" v-model="date" @input="">
   </div>
   <div class="form-group">
     <label for="newTransactionAccount">Target</label>
-    <input class="form-control" placeholder="Target"  :value="newtransaction.target" @input="updateNewTransaction">
+    <input class="form-control" placeholder="Target"  v-model="target" @input="">
   </div>
   <div class="form-group">
     <label for="newTransactionAccount">Category</label>
-    <input class="form-control" placeholder="Category" :value="newtransaction.category" @input="updateNewTransaction">
+    <input class="form-control" placeholder="Category" v-model="category" @input="">
   </div>
   <div class="form-group">
     <label for="newTransactionAccount">Memo</label>
-    <input class="form-control" placeholder="Memo" :value="newtransaction.memo" @input="updateNewTransaction">
+    <input class="form-control" placeholder="Memo" v-model="memo" @input="">
   </div>
   <div class="form-group">
     <label for="newTransactionFlow">Flow</label>
     <div class="input-group">
       <div class="input-group-addon">$</div>
-      <input class="form-control" placeholder="Flow"  :value="newtransaction.flow" @input="updateNewTransaction">
+      <input class="form-control" placeholder="Flow"  v-model="transaction.flow" @input="">
     </div>
   </div>
-    <button class="btn btn-default" v-on:click.prevent="addTransaction()">Add Transaction</button>
+    <button class="btn btn-default" v-on:click.prevent="">Add Transaction</button>
 </form>
 </template>
 
@@ -35,19 +35,26 @@
 var store = require('../store.js')
 
 module.exports = {
-  computed: {
-    newtransaction() {
-      return store.state.newtransaction
+  data: function() {
+      return { transaction: {
+      account: "",
+      date: "",
+      target: "",
+      category: "",
+      memo: "",
+      flow: 0
+    }
     }
   },
-   methods: {
-     addTransaction: store.actions.addTransaction,
 
-     updateNewTransaction: function(e) {
-       console.log(e.target, e.target.value)
-       store.actions.updateNewTransaction(e.target.value, e.target)
-     }
-   }
+   methods: {
+     addTransaction: store.actions.addTransaction
+
+  //  updateNewTransaction: function(e) {
+  //    console.log(e.target, e.target.value)
+  //    store.actions.updateNewTransaction(e.target.value, e.target)
+  //  }
+ }
 }
 </script>
 
