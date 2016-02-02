5ce98a833e199f027847e0036dd479f4d7da9c83
EricR <rasliche@gmail.com>
2016-02-01 23:22:53 -0500

    Table Head/Alternate Row

--- components/Transactions.vue
+++ components/Transactions.vue
@@ -4,20 +4,32 @@
 </header>
 <section class="row">
   <div class="col-sm-9">
-<table class="table table-striped table-condensed table-hover">
-  <tbody>
-    <tr v-for="transaction in transactions">
-      <td>{{ transaction.category }}</td>
-      <td>{{ transaction.target }}</td>
-      <td>{{ transaction.flow }}</td>
-    </tr>
-  </tbody>
-</table>
-</div>
-<div class="col-sm-3">
-<new-transaction-form>
-</new-transaction-form>
-</div>
+    <!-- Can this table be refactored out? -->
+    <table class="table table-striped table-condensed table-hover">
+      <thead>
+        <tr>
+          <th>Category</th>
+          <th>Target</th>
+          <th>Flow</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- Replace with SVG pointing to newTransForm -->
+        <tr v-show="!transactions">
+          <td>No transactions yet...</td>
+        </tr>
+        <tr v-for="transaction in transactions">
+          <td>{{ transaction.category }}</td>
+          <td>{{ transaction.target }}</td>
+          <td>{{ transaction.flow }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <div class="col-sm-3">
+    <new-transaction-form>
+    </new-transaction-form>
+  </div>
 </section>
 </template>
 
@@ -28,7 +40,11 @@ var NewTransactionForm = require('./NewTransactionForm.vue')
 module.exports = {
   components: { 'new-transaction-form': NewTransactionForm },
 
-  computed: {}
+  computed: {
+    transactions() {
+      return store.state.transactions;
+    }
+  }
 }
 </script>
 
